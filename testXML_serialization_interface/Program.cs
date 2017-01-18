a5574702da696b7f3e1b855de432820dd1853923
DESKTOP-BN1D3QV\KJfe <kollorii@gmail.com>
2017-01-18 22:10:31 +0700

    Добавьте файлы проекта.

---
+++ testXML_serialization_interface/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testXML_serialization_interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
